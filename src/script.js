3053b72cd7fee6fdab4897b214856c8176496b21
CourtneyKerr19 <court.knee219@gmail.com>
2025-06-09 12:55:06 -0500

    Added Data Structures to script.js and added comments for decriptions

---
+++ src/script.js
@@ -0,0 +1,6 @@
+//Data Structures
+let breeds = [];              // All breeds from API
+let filteredBreeds = [];      // Search-filtered breeds
+let currentCatImage = null;   // Currently displayed image
+let favorites = [];           // Array of FavoriteCat objects
+let categories = [];          // Optional categories (if using)
