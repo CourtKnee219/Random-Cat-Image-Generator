701826c36038777d8cb1c69a1e466003925f14d5
CourtneyKerr19 <court.knee219@gmail.com>
2025-06-09 12:58:20 -0500

    Add All 5 objects with their corresponding attributes

--- src/script.js
+++ src/script.js
@@ -4,3 +4,43 @@ let filteredBreeds = [];      // Search-filtered breeds
 let currentCatImage = null;   // Currently displayed image
 let favorites = [];           // Array of FavoriteCat objects
 let categories = [];          // Optional categories (if using)
+
+//Objects
+class CatImage {
+  constructor(id, url, breed, width, height) {
+    this.id = id;
+    this.url = url;
+    this.breed = breed;
+    this.width = width;
+    this.height = height;
+  }
+}
+class Breed {
+  constructor(id, name, temperament, origin) {
+    this.id = id;
+    this.name = name;
+    this.temperament = temperament;
+    this.origin = origin;
+  }
+}
+class FavoriteCat {
+  constructor(id, url, addedAt) {
+    this.id = id;
+    this.url = url;
+    this.addedAt = addedAt;
+  }
+}
+class Category {
+  constructor(id, name, description = "No description provided.") {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
+}
+class UserSettings {
+  constructor(theme, imageSize, autoRefresh) {
+    this.theme = theme;
+    this.imageSize = imageSize;
+    this.autoRefresh = autoRefresh;
+  }
+}
